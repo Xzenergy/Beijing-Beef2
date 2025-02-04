4baf0dda516659ee9e36186b3a1ad6dd01e90a7b
Xzenergy <citadel.chase@gmail.com>
2025-02-04 15:12:02 -0800

    First Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import hashlib
+from Crypto.Cipher import AES
+
+# Generates the SHA1 authentication key
+auth_password = "oasis_tech"
+auth_key = hashlib.sha1(auth_password.encode()).hexdigest()[:16]
+print("SHA1 Auth Key:", auth_key)
+
+# Generates the AES128 privacy key (first 16 bytes of SHA1 hash, this can be changed for different types of AES, i.e 256)
+privacy_password = "oasis_tech"
+aes_key = hashlib.sha1(privacy_password.encode()).digest()[:16]
+print("AES128 Privacy Key:", aes_key.hex())
