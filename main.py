cd87e2ef99bc2afd4729c200ebc0c5a579634588
Xzenergy <citadel.chase@gmail.com>
2025-02-04 15:43:25 -0800

    AES bit change to 32

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ print("SHA1 Auth Key:", auth_key)
 
 # Generates the AES128 privacy key (first 16 bytes of SHA1 hash, this can be changed for different types of AES, i.e 256)
 privacy_password = "sample_password2"
-aes_key = hashlib.sha1(privacy_password.encode()).digest()[:16]
+aes_key = hashlib.sha1(privacy_password.encode()).digest()[:32]
 print("AES128 Privacy Key:", aes_key.hex())
