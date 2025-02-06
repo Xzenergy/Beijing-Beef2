7e6208b9dd44aa5bbb0bf24ef8c96629f170f2c9
Xzenergy <citadel.chase@gmail.com>
2025-02-06 10:55:13 -0800

    Changed AES bit length to 12

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import hashlib
 from Crypto.Cipher import AES
 
 # Generates the SHA1 authentication key
-auth_password = "sample_password"
+auth_password = "COF_canseevet_25!"
 auth_key = hashlib.sha1(auth_password.encode()).hexdigest()[:16]
 print("SHA1 Auth Key:", auth_key)
 
 # Generates the AES128 privacy key (first 16 bytes of SHA1 hash, this can be changed for different types of AES, i.e 256)
-privacy_password = "sample_password2"
-aes_key = hashlib.sha1(privacy_password.encode()).digest()[:32]
+privacy_password = "fallonvetclinic25!"
+aes_key = hashlib.sha1(privacy_password.encode()).digest()[:12]
 print("AES128 Privacy Key:", aes_key.hex())
