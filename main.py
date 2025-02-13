c6056731e408ae9845783c08f7833c224973f830
Xzenergy <citadel.chase@gmail.com>
2025-02-13 08:36:20 -0800

    Changed back to default passwords

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import hashlib
 from Crypto.Cipher import AES
 
 # Generates the SHA1 authentication key
-auth_password = "sample_password"
+auth_password = "input password"
 auth_key = hashlib.sha1(auth_password.encode()).hexdigest()[:16]
 print("SHA1 Auth Key:", auth_key)
 
 # Generates the AES128 privacy key (first 16 bytes of SHA1 hash, this can be changed for different types of AES, i.e 256)
-privacy_password = "sample_password2"
+privacy_password = "input password"
 aes_key = hashlib.sha1(privacy_password.encode()).digest()[:12]
 print("AES128 Privacy Key:", aes_key.hex())
